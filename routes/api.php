b5ca1a3572a208c753d6794edbcd3f9fecb8be30
Fernast <fernandoecueto@gmail.com>
2019-01-04 16:36:18 -0500

    Creando los end-points para la apiRestful usando rutas de recurso

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,15 @@ use Illuminate\Http\Request;
 
 /*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-	//Ruta de puraba que hace uso del middelware llamado auth y guard llamado api
+	//Ruta de prueba que hace uso del middelware llamado auth y guard llamado api
 	//Quiere decir que si intentamos acceder a la ruta /user de la api debemos estar autenticados con un token
     return $request->user();
 });
 */
 
+Route::resource('buyers', 'Buyer\BuyerController', ['only' => ['index', 'show']]);
+Route::resource('categories', 'Category\CategoryController', ['except' => ['create', 'edit']]);
+Route::resource('products', 'Product\ProductController', ['only' => ['index', 'show']]);
+Route::resource('transactions', 'Transaction\TransactionController', ['only' => ['index', 'show']]);
+Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show']]);
+Route::resource('users', 'User\UserController', ['except' => ['create', 'edit']]);
