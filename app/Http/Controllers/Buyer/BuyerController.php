06bca4617f463f3e6ac4ca10c022762fe7712275
Fernast <fernandoecueto@gmail.com>
2019-01-07 23:02:21 -0500

    Implementando el método index del BuyerController

--- app/Http/Controllers/Buyer/BuyerController.php
+++ app/Http/Controllers/Buyer/BuyerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Buyer;
 
+use App\Buyer;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -14,29 +15,12 @@ class BuyerController extends Controller
      */
     public function index()
     {
-        //
+        $compradores = Buyer::has('transactions')->get(); //Trae los usuarios que tengan trasacciones (buyers) ya que hereda de users
+        //has recibe el nombre de una relacion que tenga el modelo
+
+        return response()->json(['data' => $compradores], 200);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
     /**
      * Display the specified resource.
@@ -46,40 +30,9 @@ class BuyerController extends Controller
      */
     public function show($id)
     {
-        //
+        //$comprador = Buyer::findOrFail($id)->has('transactions');
+
+        //return response()->json()
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
