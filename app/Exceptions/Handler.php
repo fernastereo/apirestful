8b52b830a0b251d4c40655cade6ca269a5362cb4
Fernast <fernandoecueto@gmail.com>
2019-01-08 01:54:34 -0500

    Controlando la eliminacion de recursos con relaciones

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Exception;
 use App\Traits\ApiResponser;
+use Illuminate\Database\QueryException;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Auth\Access\AuthorizationException;
@@ -85,6 +86,14 @@ class Handler extends ExceptionHandler
         if ($exception instanceof HttpException) {
             return $this->errorResponse($exception->getMessage(), $exception->getStatusCode());
         } 
+
+        if ($exception instanceof QueryException) {
+            $codigo = $exception->errorInfo[1];
+
+            if ($codigo == 1451) {
+                return $this->errorResponse('no se puede eliminar el recurso porque está relacionado con otro', 409);
+            }
+        }
 
         return parent::render($request, $exception);
     }
