640a8fd7660e1d31f137c9b4c3687b7c7eca3253
Fernast <fernandoecueto@gmail.com>
2019-01-09 01:11:53 -0500

    Preparando todos los modelos para usar SoftDeletes

--- app/Product.php
+++ app/Product.php
@@ -6,12 +6,17 @@ use App\Seller;
 use App\Category;
 use App\Transaction;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-	const PRODUCTO_DISPONIBLE = 'disponible';
-	const PRODUCTO_NO_DISPONIBLE = 'no disponible';
+    use SoftDeletes;
 
+    
+    const PRODUCTO_DISPONIBLE = 'disponible';
+    const PRODUCTO_NO_DISPONIBLE = 'no disponible';
+
+    protected $dates = ['delete_at'];
     protected $fillable = [
     	'name',
     	'description',
