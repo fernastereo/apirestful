5e2e8afb79f303d9f8959839bf4d4aa5efddaaf4
Fernast <fernandoecueto@gmail.com>
2019-01-09 15:15:46 -0500

    Implementación del método index TransactionSellerController

--- routes/api.php
+++ routes/api.php
@@ -27,6 +27,7 @@ Route::resource('products', 'Product\ProductController', ['only' => ['index', 's
 
 Route::resource('transactions', 'Transaction\TransactionController', ['only' => ['index', 'show']]);
 Route::resource('transactions.categories', 'Transaction\TransactionCategoryController', ['only' => ['index']]);
+Route::resource('transactions.sellers', 'Transaction\TransactionSellerController', ['only' => ['index']]);
 
 Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show']]);
 Route::resource('users', 'User\UserController', ['except' => ['create', 'edit']]);
