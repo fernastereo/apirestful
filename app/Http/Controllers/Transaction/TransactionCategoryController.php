75580c027527f3772ec7240e535b790a633e5995
Fernast <fernandoecueto@gmail.com>
2019-01-09 15:06:23 -0500

    Implementación del método index TransactionCategoryController

--- app/Http/Controllers/Transaction/TransactionCategoryController.php
+++ app/Http/Controllers/Transaction/TransactionCategoryController.php
@@ -4,17 +4,19 @@ namespace App\Http\Controllers\Transaction;
 
 use App\Transaction;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\ApiController;
 
-class TransactionCategoryController extends Controller
+class TransactionCategoryController extends ApiController
 {
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Transaction $transaction)
     {
-        //
+        $categories = $transaction->product->categories;
+
+        return $this->showAll($categories);
     }
 }
