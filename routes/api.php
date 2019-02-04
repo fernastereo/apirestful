92401781fe22d874302e11d9bdc839777c73e352
fernastereo <fernandoecueto@gmail.com>
2019-02-04 18:22:44 -0500

    Implementando el método index en CategoryBuyerController

--- routes/api.php
+++ routes/api.php
@@ -31,6 +31,7 @@ Route::resource('categories', 'Category\CategoryController', ['except' => ['crea
 Route::resource('categories.products', 'Category\CategoryProductController', ['only' => ['index']]);
 Route::resource('categories.sellers', 'Category\CategorySellerController', ['only' => ['index']]);
 Route::resource('categories.transactions', 'Category\CategoryTransactionController', ['only' => ['index']]);
+Route::resource('categories.buyers', 'Category\CategoryBuyerController', ['only' => ['index']]);
 
 Route::resource('products', 'Product\ProductController', ['only' => ['index', 'show']]);
 
