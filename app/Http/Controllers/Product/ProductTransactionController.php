94d4affd168c26821b4246ad36586b63044a9f30
fernastereo <fernandoecueto@gmail.com>
2019-02-25 12:43:18 -0500

    Creando y usando un middleware para resolver el problema de los nombres de campo en las transformaciones con store o update

--- app/Http/Controllers/Product/ProductTransactionController.php
+++ app/Http/Controllers/Product/ProductTransactionController.php
@@ -5,9 +5,15 @@ namespace App\Http\Controllers\Product;
 use App\Product;
 use Illuminate\Http\Request;
 use App\Http\Controllers\ApiController;
+use App\Transformers\TransactionTransformer;
 
 class ProductTransactionController extends ApiController
 {
+    public function __construct(){
+        parent::__construct();
+
+        $this->middleware('transform.input:' . TransactionTransformer::class)->only(['store']);
+    }
     /**
      * Display a listing of the resource.
      *
